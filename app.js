64efae8d9f4f6b464773b4633bc483b62c45273f
Manish Pandit <manish.kumer.pandit@gmail.com>
2018-06-10 12:57:23 -0400

    Add pagination

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/**
+ * app.js
+*/
+
 var express = require( 'express' );
 var app = express();
 var MongoClient = require( 'mongodb' ).MongoClient;
@@ -8,6 +12,9 @@ var bodyParser = require( 'body-parser' );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
+/**
+ * Defining header for API
+*/
 app.use( function( req, res, next ) {
     res.header( "Access-Control-Allow-Origin", "*" );
     res.header( "Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept, Authorization" );
@@ -27,31 +34,46 @@ app.use('/assets', express.static('assets'));
 var apiRoutes = require( './api/router' );
 app.use( '/api', apiRoutes );
 
-app.get( '/', ( req, res, next ) => {
+/**
+ * Route to add an image entity
+*/
+app.get( '/add', ( req, res, next ) => {
+    res.render( 'add', { title: 'File API' } )
+} );
+
+/**
+ * Route to list images with pagination
+*/
+app.get( ['/', '/:page'], ( req, res, next ) => {
     MongoClient.connect( dbUrl, ( dberr, client ) => {
-        var perpage = 4;
+        var perpage = 6;
         var page = req.params.page || 1;
         if( dberr) {
             console.log( dberr );
         } else {
             var db = client.db( 'nodealbum' );
             db.collection( 'singleimages' ).find( {} )
-            //.skip( ( perpage * page ) - perpage ).limit( perpage )
+            .skip( ( perpage * page ) - perpage ).limit( perpage )
             .toArray( function( err, result ) {
                 if( err ) {
-                    res.json( err );
+                    console.log( err );
                 } else {
-                    //console.log( result.length );
-                    //res.status( 200 ).json( { 'data': result } );
-                    res.render( 'index', { title: 'File API', 'data': result } );
+                    db.collection( 'singleimages' ).count(function( er, count ) {
+                        if( er ) {
+                            console.log( er );
+                        } else {
+                            res.render( 'index', { 
+                                title: 'File API',
+                                data: result,
+                                current: page,
+                                pages: Math.ceil( count/perpage )
+                            } );
+                        }
+                    });
                 }
             } );
         }
     } );
-} );
-
-app.get( '/add', ( req, res, next ) => {
-    res.render( 'add', { title: 'File API' } )
 } );
 
 app.listen( port, function() {
